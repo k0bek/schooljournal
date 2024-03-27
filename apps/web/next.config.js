9c029014b3798c5d2cfa6e74955814156b2a8c4b
k0bek <kubakiebala@gmail.com>
2024-03-27 18:44:36 +0100

    fix: declare API_KEY in turbo.json to fix deployment error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -19,4 +19,11 @@ module.exports = {
 			},
 		],
 	},
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		config.externals.push({
+			'utf-8-validate': 'commonjs utf-8-validate',
+			bufferutil: 'commonjs bufferutil',
+		});
+		return config;
+	},
 };
